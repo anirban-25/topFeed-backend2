8264dfb77b0a916eb551653a91a59b9771bacdba
Anirban <anirban.ghosh.aiml25@heritageit.edu.in>
2024-09-14 23:50:56 +0530

    push

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,8 @@
-import serverless from 'serverless-http';
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import redditRoutes from '../src/routes/redditRoutes.js';
+import serverless from "serverless-http";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import redditRoutes from "../src/routes/redditRoutes.js";
 
 dotenv.config();
 
@@ -11,19 +11,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'API is working' });
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API is working" });
 });
 
-if (typeof redditRoutes === 'function') {
-  app.use('/api/reddit', redditRoutes);
-} else {
-  console.error('redditRoutes is not a function:', redditRoutes);
-}
+// if (typeof redditRoutes === 'function') {
+app.use("/api/reddit", redditRoutes);
+// } else {
+//   console.error('redditRoutes is not a function:', redditRoutes);
+// }
 
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).json({ error: 'Internal Server Error' });
+  res.status(500).json({ error: "Internal Server Error" });
 });
 
 export const handler = serverless(app);
