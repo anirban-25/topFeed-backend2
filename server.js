df5c98f8ff203788d067c6eb13b3eaf6a16c0a5f
Anirban <anirban.ghosh.aiml25@heritageit.edu.in>
2024-09-14 23:10:11 +0530

    push-again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import serverless from 'serverless-http';
 import express from 'express';
 import dotenv from 'dotenv';
-import redditRoutes from '../src/routes/redditRoutes.js';
+import redditRoutes from './src/routes/redditRoutes.js';
 
 dotenv.config();
 
